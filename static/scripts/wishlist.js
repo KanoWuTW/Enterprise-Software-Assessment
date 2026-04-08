a5dca88c5acf5e47b410b075637d96cb112699c4
Chia-Chen Wu <kevin0211722@gmail.com>
2026-04-08 22:02:19 +0100

    fixed add to basket btn in wishlist

--- static/scripts/wishlist.js
+++ static/scripts/wishlist.js
@@ -1,6 +1,26 @@
-bindRemoveBtns()
 // Set the default page value for search/pagination
 document.getElementById("defaultpage").value = 1;
+
+function bindBaketBtns() {
+    const ul = document.getElementById("wishlist");
+    ul.querySelectorAll("li").forEach(li => {
+        // Get the product id from the link
+        const url = new URL(li.querySelector("a").href);
+        const params = new URLSearchParams(url.search);
+        // Add click event to the remove button
+        li.querySelector(".btn.btn-success").addEventListener("click", () => {
+            fetch(`/item/addbasket?id=${params.get("id")}`)
+                .then((response) => {
+                    response.json().then((data) => {
+                        if (data.in_stock) {
+                            console.log("successfully added to basket.")
+                        }
+                    });
+                });
+        });
+    });
+}
+
 
 // Bind click events for all "Remove from wishlist" buttons
 function bindRemoveBtns() {
@@ -26,3 +46,4 @@ function bindRemoveBtns() {
 
 // Initial binding for page load
 bindRemoveBtns();
+bindBaketBtns()
