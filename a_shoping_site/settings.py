152e4ff6d95b74238a1004563412cfe9314ffc5b
Kevin Wu <100563551+KanoWuTW@users.noreply.github.com>
2026-04-20 22:29:16 +0100

    Remove secret key from settings.py
    Removed the secret key for security reasons.

--- a_shoping_site/settings.py
+++ a_shoping_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q(o)xs5v021ha1l_r8z@#vhr!u2$2co_1doxjkr@r#8087m49%"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
